2dab22bcf41f74ed9c3a0cae152a3191f9240bd5
pedroronchini <pe-ronchini@hotmail.com>
2022-07-19 11:49:00 -0300

    create class cano and chao

--- flappybird.py
+++ flappybird.py
@@ -89,11 +89,63 @@ class Passaro:
 
     def get_mask(self):
         pygame.mask.from_surface(self.imagem)
-        
+
 
 class Cano:
-    pass
+    DISTANCIA = 200
+    VELOCIDADE = 5
+
+    def __init__(self, x):
+        self.x = x
+        self.altura = 0
+        self.posicao_top = 0
+        self.posicao_base = 0
+        self.CANO_TOP = pygame.transform.flip(IMAGEM_CANO, False, True)
+        self.CANO_BASE = IMAGEM_CANO
+        self.passou = False
+        self.definir_altura()
+
+    def definir_altura(self):
+        self.altura = random.randrange(50, 450)
+        self.posicao_top = self.altura - self.CANO_TOP.get_height()
+        self.posicao_base = self.altura + self.DISTANCIA
+
+    def mover(self):
+        self.x -= self.VELOCIDADE
+
+    def desenhar(self, tela):
+        tela.blit(self.CANO_TOP, (self.x, self.posicao_top))
+        tela.blit(self.CANO_BASE, (self.x, self.posicao_base))
+
+    def colidir(self, passaro):
+        passaro_mask = passaro.get_mask()
+        top_mask = pygame.mask.from_surface(self.CANO_TOP)
+        base_mask = pygame.mask.from_surface(self.CANO_BASE)
+
+        distancia_top = (self.x - passaro.x, self.posicao_top - round(passaro.y))
+        distancia_base = (self.x - passaro.x, self.posicao_base - round(passaro.y))
+
+        top_ponto = passaro_mask.overlap(top_mask, distancia_top)
+        base_ponto = passaro_mask.overlap(base_mask, distancia_base)
+
+        if base_ponto or top_ponto:
+            return True
+        else:
+            return False
 
 
 class Chao:
-    pass
+    VELOCIDADE = 5
+    LARGURA = IMAGEM_CHAO.get_width()
+    IMAGEM = IMAGEM_CHAO
+
+    def __init__(self, y):
+        self.y = y
+        self.x0 = 0
+        self.x1 = self.LARGURA
+
+    def mover(self):
+        self.x0 -= self.VELOCIDADE
+        self.x1 -= self.VELOCIDADE
+
+
